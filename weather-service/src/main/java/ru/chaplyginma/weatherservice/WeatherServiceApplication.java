6cab513dbe2cda81bb23e8bc69a6ab08229a52f4
GibbedHead <chaplygin-ma@yandex.ru>
2024-05-08 17:56:56 +0300

    - Переделал запуск приложения на CommandLineRunner

--- weather-service/src/main/java/ru/chaplyginma/weatherservice/WeatherServiceApplication.java
+++ weather-service/src/main/java/ru/chaplyginma/weatherservice/WeatherServiceApplication.java
@@ -1,25 +1,24 @@
 package ru.chaplyginma.weatherservice;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import ru.chaplyginma.weatherservice.weather.WeatherService;
 
 @RequiredArgsConstructor
 @SpringBootApplication
 @EnableAsync
-public class WeatherServiceApplication {
+public class WeatherServiceApplication implements CommandLineRunner {
     private final WeatherService weatherService;
 
     public static void main(String[] args) {
         SpringApplication.run(WeatherServiceApplication.class, args);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void print() {
+    @Override
+    public void run(String... args) {
         weatherService.asyncPrintForCity("Moscow");
         weatherService.printForCity("Moscow");
         weatherService.asyncPrintForCity("Rostov-on-Don");
