b7efe3dbf6c670947c027713ff1dc8dc9f1af074
GibbedHead <chaplygin-ma@yandex.ru>
2024-05-09 16:52:38 +0300

    - интерфейс для сервиса - в реализации сервиса вынес логику получения значения из базы в зависимости от типа аггрегации в отдельный метод

--- metrics/src/main/java/ru/chaplyginma/metrics/service/MethodExecutionService.java
+++ metrics/src/main/java/ru/chaplyginma/metrics/service/MethodExecutionService.java
@@ -1,75 +1,20 @@
 package ru.chaplyginma.metrics.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.mapstruct.factory.Mappers;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.chaplyginma.metrics.aggregateType.AggregateType;
 import ru.chaplyginma.metrics.dto.AddMethodExecutionDto;
 import ru.chaplyginma.metrics.dto.ResponseMethodExecutionDto;
-import ru.chaplyginma.metrics.mapper.MethodExecutionMapper;
 import ru.chaplyginma.metrics.model.MethodExecution;
-import ru.chaplyginma.metrics.repository.MethodExecutionRepository;
 
 import java.time.LocalDateTime;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class MethodExecutionService {
-    private final MethodExecutionRepository methodExecutionRepository;
-    private final MethodExecutionMapper methodExecutionMapper = Mappers.getMapper(MethodExecutionMapper.class);
+public interface MethodExecutionService {
+    MethodExecution save(AddMethodExecutionDto addMethodExecutionDto);
 
-    @Transactional
-    public MethodExecution save(AddMethodExecutionDto addMethodExecutionDto) {
-        MethodExecution savedMethodExecution = methodExecutionRepository.save(
-                methodExecutionRepository.save(
-                        methodExecutionMapper.addDtoToMethodExecution(addMethodExecutionDto)
-                )
-        );
-        log.info("Added method execution: {}", savedMethodExecution);
-        return savedMethodExecution;
-    }
-
-    @Transactional(readOnly = true)
-    public ResponseMethodExecutionDto aggregateExecutionsByClassName(
+    ResponseMethodExecutionDto aggregateExecutionsByClassName(
             String className, LocalDateTime startDate, LocalDateTime endDate, AggregateType aggregateType
-    ) {
-        ResponseMethodExecutionDto responseMethodExecutionDto = new ResponseMethodExecutionDto();
-        responseMethodExecutionDto.setClassName(className);
-        responseMethodExecutionDto.setAggregateType(aggregateType);
-        Double aggregateValue = switch (aggregateType) {
-            case AVERAGE ->
-                    methodExecutionRepository.calculateAverageExecutionTimeByClassName(className, startDate, endDate);
-            case TOTAL ->
-                    methodExecutionRepository.calculateTotalExecutionTimeByClassName(className, startDate, endDate);
-            case MIN -> methodExecutionRepository.calculateMinExecutionTimeByClassName(className, startDate, endDate);
-            case MAX -> methodExecutionRepository.calculateMaxExecutionTimeByClassName(className, startDate, endDate);
-        };
-        responseMethodExecutionDto.setAggregateValue(aggregateValue);
-        return responseMethodExecutionDto;
-    }
+    );
 
-    @Transactional(readOnly = true)
-    public ResponseMethodExecutionDto aggregateExecutionsByClassNameAndMethodName(
+    ResponseMethodExecutionDto aggregateExecutionsByClassNameAndMethodName(
             String className, String methodName, LocalDateTime startDate, LocalDateTime endDate, AggregateType aggregateType
-    ) {
-        ResponseMethodExecutionDto responseMethodExecutionDto = new ResponseMethodExecutionDto();
-        responseMethodExecutionDto.setClassName(className);
-        responseMethodExecutionDto.setMethodName(methodName);
-        responseMethodExecutionDto.setAggregateType(aggregateType);
-        Double aggregateValue = switch (aggregateType) {
-            case AVERAGE ->
-                    methodExecutionRepository.calculateAverageExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
-            case TOTAL ->
-                    methodExecutionRepository.calculateTotalExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
-            case MIN ->
-                    methodExecutionRepository.calculateMinExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
-            case MAX ->
-                    methodExecutionRepository.calculateMaxExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
-        };
-        responseMethodExecutionDto.setAggregateValue(aggregateValue);
-        return responseMethodExecutionDto;
-    }
+    );
 }
---
+++ metrics/src/main/java/ru/chaplyginma/metrics/service/MethodExecutionServiceImpl.java
@@ -0,0 +1,94 @@
+package ru.chaplyginma.metrics.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.chaplyginma.metrics.aggregateType.AggregateType;
+import ru.chaplyginma.metrics.dto.AddMethodExecutionDto;
+import ru.chaplyginma.metrics.dto.ResponseMethodExecutionDto;
+import ru.chaplyginma.metrics.mapper.MethodExecutionMapper;
+import ru.chaplyginma.metrics.model.MethodExecution;
+import ru.chaplyginma.metrics.repository.MethodExecutionRepository;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MethodExecutionServiceImpl implements MethodExecutionService {
+    private final MethodExecutionRepository methodExecutionRepository;
+    private final MethodExecutionMapper methodExecutionMapper = Mappers.getMapper(MethodExecutionMapper.class);
+
+    @Transactional
+    public MethodExecution save(AddMethodExecutionDto addMethodExecutionDto) {
+        MethodExecution savedMethodExecution = methodExecutionRepository.save(
+                methodExecutionRepository.save(
+                        methodExecutionMapper.addDtoToMethodExecution(addMethodExecutionDto)
+                )
+        );
+        log.info("Added method execution: {}", savedMethodExecution);
+        return savedMethodExecution;
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseMethodExecutionDto aggregateExecutionsByClassName(
+            String className, LocalDateTime startDate, LocalDateTime endDate, AggregateType aggregateType
+    ) {
+        ResponseMethodExecutionDto responseMethodExecutionDto = new ResponseMethodExecutionDto();
+        responseMethodExecutionDto.setClassName(className);
+        responseMethodExecutionDto.setAggregateType(aggregateType);
+        Double aggregateValue = getAggregateValueByClassName(aggregateType, className, startDate, endDate);
+        responseMethodExecutionDto.setAggregateValue(aggregateValue);
+        return responseMethodExecutionDto;
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseMethodExecutionDto aggregateExecutionsByClassNameAndMethodName(
+            String className, String methodName, LocalDateTime startDate, LocalDateTime endDate, AggregateType aggregateType
+    ) {
+        ResponseMethodExecutionDto responseMethodExecutionDto = new ResponseMethodExecutionDto();
+        responseMethodExecutionDto.setClassName(className);
+        responseMethodExecutionDto.setMethodName(methodName);
+        responseMethodExecutionDto.setAggregateType(aggregateType);
+        Double aggregateValue = getAggregateValueByClassNameAndMethodName(aggregateType, className, methodName, startDate, endDate);
+        responseMethodExecutionDto.setAggregateValue(aggregateValue);
+        return responseMethodExecutionDto;
+    }
+
+    private Double getAggregateValueByClassName(
+            AggregateType aggregateType,
+            String className,
+            LocalDateTime startDate,
+            LocalDateTime endDate
+    ) {
+        return switch (aggregateType) {
+            case AVERAGE ->
+                    methodExecutionRepository.calculateAverageExecutionTimeByClassName(className, startDate, endDate);
+            case TOTAL ->
+                    methodExecutionRepository.calculateTotalExecutionTimeByClassName(className, startDate, endDate);
+            case MIN -> methodExecutionRepository.calculateMinExecutionTimeByClassName(className, startDate, endDate);
+            case MAX -> methodExecutionRepository.calculateMaxExecutionTimeByClassName(className, startDate, endDate);
+        };
+    }
+
+    private Double getAggregateValueByClassNameAndMethodName(
+            AggregateType aggregateType,
+            String className,
+            String methodName,
+            LocalDateTime startDate,
+            LocalDateTime endDate
+    ) {
+        return switch (aggregateType) {
+            case AVERAGE ->
+                    methodExecutionRepository.calculateAverageExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
+            case TOTAL ->
+                    methodExecutionRepository.calculateTotalExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
+            case MIN ->
+                    methodExecutionRepository.calculateMinExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
+            case MAX ->
+                    methodExecutionRepository.calculateMaxExecutionTimeByClassNameAndMethodName(className, methodName, startDate, endDate);
+        };
+    }
+}
